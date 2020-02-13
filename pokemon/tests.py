069f7ff1cc931a7c231973e31e6bd0109434af3f
Abdelrahmen Ayman <abdelrahmenayman7@gmail.com>
2020-02-13 23:05:37 +0200

    feat(pokemon name options): Get name options from api and add them in pokemon viewset metadata

--- pokemon/tests.py
+++ pokemon/tests.py
@@ -1,11 +1,12 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from requests.models import Response
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 from pokemon.exceptions import PokemonDoesNotExist
-from pokemon.external_pokemon_api import retrieve_pokemon_from_api, create_ability_from_json, retrieve_pokemon_abilities
+from pokemon.external_pokemon_api import retrieve_pokemon_from_api, create_ability_from_json, \
+    retrieve_pokemon_abilities, get_pokemon_available_names
 from pokemon.factories import UserFactory, AbilityFactory, PokemonFactory
 from pokemon.models import Pokemon, Ability
 
@@ -123,6 +124,27 @@ class ExternalPokemonAPIModuleTestSuite(APITestCase):
         self.assertFalse(api_call.called)
         self.assertEqual(len(existing_pokemon.abilities.all()), len(abilities))
         self.assertEqual(existing_pokemon.abilities.first().pk, abilities[0].pk)
+
+    @patch('pokemon.external_pokemon_api.requests.get')
+    def test_get_pokemon_available_names(self, api_call):
+        mock_response = MagicMock()
+        mock_response.json = MagicMock(return_value={
+            "count": 964,
+            "next": 'null',
+            "previous": 'null',
+            "results": [
+                {
+                    "name": "bulbasaur",
+                    "url": "https://pokeapi.co/api/v2/pokemon/1/"
+                },
+                {
+                    "name": "ivysaur",
+                    "url": "https://pokeapi.co/api/v2/pokemon/2/"
+                }, ]})
+        api_call.return_value = mock_response
+
+        result = get_pokemon_available_names()
+        self.assertEqual(["bulbasaur", "ivysaur"], result)
 
 
 class ListPokemonActionTestSuite(APITestCase):
--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from digimon.views import DigimonViewSet
+from pokemon.external_pokemon_api import get_pokemon_available_names
 from pokemon.views import PokemonViewSet
+
+settings.AVAILABLE_POKEMON_NAMES = get_pokemon_available_names()
 
 PokemonRouter = DefaultRouter()
 PokemonRouter.register('pokemons', PokemonViewSet, basename='pokemon')
